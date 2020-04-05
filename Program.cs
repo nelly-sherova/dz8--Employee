8a43c5e29c39752a9c192cb643244cdb0f839f18
unknown <lvova.nikol@mail.ru>
2020-04-05 20:15:30 +0300

    Объявление класса, его полей и методов

--- Program.cs
+++ Program.cs
@@ -2,6 +2,19 @@
 
 namespace Employee
 {
+    class Employee
+    {
+        public string name;
+        public string surname;
+        public Employee(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+        public int experience;
+        public string position; 
+        public double salary;
+    }
     class Program
     {
         static void Main(string[] args)
